a7bad75ee5c21f9a3df32876642ebd76509fe1c8
Asif Syed <asiifsyed@gmail.com>
2026-01-02 20:41:54 -0800

    Steps to do next

--- main.go
+++ main.go
@@ -16,3 +16,21 @@ func main() {
 	webDispose := startServer(cfg)
 	defer webDispose(ctx)
 }
+
+/*
+	1. Connect to postgres with retry mechanism and logging
+	2. Create MODEL for persons
+	3. API endpoints to CRUD persons with request validations
+	4. Login mechanism with JWT token, check in headers and also for token
+	5. Permission check to perform Write operations on persons api using a middleware
+	6. Error middleware to handle error globally
+	7. Error classes and different types of errors
+	8. Logging middleware
+	9. Tracing middleware
+	10. CORS Middleware
+	11. Combine all necessary middlewares into one
+	12. Internationalization
+	13. Swagger docs
+	14. Docker compose
+	15. Make file
+*/
--- server.go
+++ server.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"rest_api_poc/config"
 	"rest_api_poc/logger"
 	"rest_api_poc/router"
+	"strings"
 	"time"
+
+	"github.com/fatih/color"
 )
 
 // startServer starts the HTTP server and handles graceful shutdown
@@ -20,8 +24,8 @@ func startServer(cfg *config.Config) func(ctx context.Context) error {
 		ReadTimeout:  cfg.WebServer.ReadTimeout,
 		WriteTimeout: cfg.WebServer.WriteTimeout,
 	}
-
-	logger.InfoBlock("Starting server on %s", addr)
+	PrintWelcome(addr)
+	// logger.InfoBlock("Starting server on %s", addr)
 
 	// This blocks until the server stops
 	err := srv.ListenAndServe()
@@ -42,5 +46,18 @@ func startServer(cfg *config.Config) func(ctx context.Context) error {
 		}
 		logger.Success("Server stopped gracefully")
 		return nil
+	}
+}
+
+func PrintWelcome(addr string) {
+	c := color.New(color.FgHiCyan)
+	srvMsg := fmt.Sprintf("🚀🚀🚀 Starting Axil server on %s 🚀🚀🚀", addr)
+	line := strings.Repeat("*", len(srvMsg))
+	for i := 0; i < 2; i++ {
+		c.Println(line)
+	}
+	c.Printf("**  %s  **\n", srvMsg)
+	for i := 0; i < 2; i++ {
+		c.Println(line)
 	}
 }
